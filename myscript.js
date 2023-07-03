e32dbb5b0012e2f9017e5ad7755e106fd24a3025
vemulashashidar <39757304+vemulashashidar@users.noreply.github.com>
2023-07-03 23:45:41 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myModal");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
